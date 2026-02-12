80eadc4f8dfd5a9fd9e871a8dcfe4266c4630497
JEIPAO <jlepaopao305@gmail.com>
2026-02-12 22:25:58 +0800

    Initial commit for GitHub Pages deployment

---
+++ love letter 2.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
+  $('#close').hide();
+
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+    btn_open.hide();
+      $('#close').show();
+  }
+
+});
